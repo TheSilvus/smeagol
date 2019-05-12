7148e93ce40f1373f0e72324612d26cc910a6cf8
TheSilvus <renkenor@gmail.com>
2019-05-12 21:16:17 +0000

    Set up basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate log;
+extern crate pretty_env_logger;
+extern crate warp;
+
+mod smeagol;
+use smeagol::Smeagol;
+
 fn main() {
-    println!("Hello, world!");
+    pretty_env_logger::init_custom_env("SMEAGOL_LOG");
+
+    let smeagol = Smeagol::new();
+    smeagol.start();
 }
---
+++ src/smeagol.rs
@@ -0,0 +1,30 @@
+use log::{debug, info};
+use warp::{Filter, Reply};
+
+pub struct Smeagol {}
+impl Smeagol {
+    pub fn new() -> Smeagol {
+        debug!("Initializing");
+        Smeagol {}
+    }
+
+    pub fn start(self) {
+        info!("Starting on 127.0.0.1:8000");
+
+        warp::serve(self.routes()).run(([127, 0, 0, 1], 8000));
+    }
+
+    fn routes(&self) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+        return self.index().or(self.something());
+    }
+
+    fn index(&self) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+        warp::path::end().map(|| "Hello!")
+    }
+
+    fn something(&self) -> impl Filter<Extract = impl Reply, Error = warp::Rejection> + Clone {
+        warp::path::path("test")
+            .and(warp::path::end())
+            .map(|| "Hello!")
+    }
+}
