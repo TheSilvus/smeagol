5112801803dc789da1ca4edfcbe79aeb2e04f98d
TheSilvus <renkenor@gmail.com>
2019-05-23 21:03:38 +0000

    Removed unnecessary extern crate declarations

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
-extern crate comrak;
-extern crate git2;
-extern crate handlebars;
-extern crate log;
-extern crate percent_encoding;
-extern crate pretty_env_logger;
-extern crate serde;
-extern crate serde_json;
-#[cfg(test)]
-extern crate tempdir;
-extern crate toml;
-extern crate warp;
-
 mod smeagol;
 use smeagol::Smeagol;
 
